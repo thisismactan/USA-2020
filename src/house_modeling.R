948590bdcb2329ebebd43fa2ddc3e47a8c90653f
thisismactan <mactan@gmail.com>
2020-05-03 23:59:44 -0400

    Settled on simple linear regression model for House (for now)

--- src/house_modeling.R
+++ src/house_modeling.R
@@ -1,26 +1,129 @@
 source("src/shape_house_data.R")
 
+set.seed(2020)
+
 # Models
 ## Linear regression
-house_lm <- lm(margin ~ last_margin + natl_margin + last_natl_margin + state_margin + last_state_margin + pres_year * incumbent_running, 
-               data = house_results_2party_filtered)
-summary(house_lm)
+house_lm_pre_2016 <- lm(margin ~ natl_margin + last_natl_margin + last_margin + incumbency_change, 
+               data = house_results_2party_filtered %>% filter(year < 2016))
+summary(house_lm_pre_2016)
 
 ## Random forest
-house_rf <- randomForest(formula = margin ~ last_margin + natl_margin + last_natl_margin + state_margin + last_state_margin + pres_year + 
-                           incumbency_change, data = house_results_2party_filtered, ntree = 150, importance = TRUE)
-house_rf
+house_rf_pre_2016 <- randomForest(formula = margin ~ last_margin + natl_margin + last_natl_margin + state_margin + last_state_margin + pres_year + 
+                           incumbency_change, data = house_results_2party_filtered %>% filter(year < 2016), ntree = 200, importance = TRUE)
+house_rf_pre_2016
 
 ## Gradient boosted trees
 house_results_matrix <- model.matrix(~0 + last_margin + natl_margin + last_natl_margin + state_margin + last_state_margin + pres_year + 
                                        incumbency_change, data = house_results_2party_filtered)
 house_results_dmatrix <- xgb.DMatrix(data = house_results_matrix, label = house_results_2party_filtered$margin)
 
-house_xgb_cv <- xgb.cv(params = list(objective = "reg:squarederror",
-                                     eta = 0.05, 
-                                     max_depth = 3,
-                                     nthread = 10),
+xgb_params_list <- list(objective = "reg:squarederror",
+                        eta = 0.09, 
+                        max_depth = 3,
+                        nthread = 10,
+                        alpha = 0.2)
+
+house_xgb_cv <- xgb.cv(params = xgb_params_list,
                        data = house_results_dmatrix,
-                       nrounds = 500,
+                       nrounds = 1000,
                        nfold = 10,
                        early_stopping_rounds = 20)
+
+# Estimating error from 2016 model
+house_results_pre_2016_matrix <- model.matrix(~0 + last_margin + natl_margin + last_natl_margin + state_margin + last_state_margin + pres_year + 
+                                               incumbency_change, data = house_results_2party_filtered %>% filter(year < 2016))
+house_results_pre_2016_dmatrix <- xgb.DMatrix(data = house_results_pre_2016_matrix, 
+                                              label = house_results_2party_filtered %>% filter(year < 2016) %>% pull(margin))
+
+house_pre_2016_xgb <- xgb.train(params = xgb_params_list, data = house_results_pre_2016_dmatrix, nrounds = 261, nfold = 10)
+
+house_results_2016_matrix <- model.matrix(~0 + last_margin + natl_margin + last_natl_margin + state_margin + last_state_margin + pres_year + 
+                                            incumbency_change, data = house_results_2party_filtered %>% filter(year == 2016))
+house_results_2016_dmatrix <- xgb.DMatrix(data = house_results_2016_matrix)
+
+# Model evaluation
+## Linear regression
+house_results_2party_filtered %>%
+  ungroup() %>%
+  filter(year == 2016) %>%
+  mutate(pred = predict(house_lm_pre_2016, newdata = .),
+         residual = pred - margin) %>%
+  summarise(avg_residual = mean(residual),
+            residual_sd = sd(residual),
+            rmse = sqrt(mean(residual^2)),
+            mad = mean(abs(residual)),
+            smape = mean(2 * abs(residual) / (abs(pred) + abs(margin))))
+
+house_results_2party_filtered %>%
+  ungroup() %>%
+  filter(year == 2016) %>%
+  mutate(pred = predict(house_lm_pre_2016, newdata = .),
+         residual = pred - margin) %>%
+  ggplot(aes(x = margin, y = residual)) +
+  geom_point() +
+  geom_smooth(method = "loess") +
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title = "Linear regression prediction error on 2016 House results", x = "Actual margin",
+       y = "Residual")
+
+## Random forest
+house_results_2party_filtered %>%
+  ungroup() %>%
+  filter(year == 2016) %>%
+  mutate(pred = predict(house_rf_pre_2016, newdata = .),
+         residual = pred - margin) %>%
+  summarise(avg_residual = mean(residual),
+            residual_sd = sd(residual),
+            rmse = sqrt(mean(residual^2)),
+            mad = mean(abs(residual)),
+            smape = mean(2 * abs(residual) / (abs(pred) + abs(margin))))
+
+house_results_2party_filtered %>%
+  ungroup() %>%
+  filter(year == 2016) %>%
+  mutate(pred = predict(house_rf_pre_2016, newdata = .),
+         residual = pred - margin) %>%
+  ggplot(aes(x = margin, y = residual)) +
+  geom_point() +
+  geom_smooth(method = "loess") +
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title = "Random forest prediction error on 2016 House results", x = "Actual margin",
+       y = "Residual")
+
+## XGBoost
+house_results_2party_filtered %>%
+  ungroup() %>%
+  filter(year == 2016) %>%
+  mutate(pred = predict(house_pre_2016_xgb, newdata = house_results_2016_dmatrix),
+         residual = pred - margin) %>%
+  summarise(avg_residual = mean(residual),
+            residual_sd = sd(residual),
+            rmse = sqrt(mean(residual^2)),
+            mad = mean(abs(residual)),
+            smape = mean(2 * abs(residual) / (abs(pred) + abs(margin))))
+
+house_results_2party_filtered %>%
+  ungroup() %>%
+  filter(year == 2016) %>%
+  mutate(pred = predict(house_pre_2016_xgb, newdata = house_results_2016_dmatrix),
+         residual = pred - margin) %>%
+  ggplot(aes(x = margin, y = residual)) +
+  geom_point() +
+  geom_smooth(method = "loess") +
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title = "XGBoost prediction error on 2016 House results", x = "Actual margin",
+       y = "Residual")
+
+# 2018 model
+house_lm <- lm(margin ~ natl_margin + last_natl_margin + last_margin + incumbency_change, data = house_results_2party_filtered)
+house_sigma <- house_results_2party_filtered %>%
+  ungroup() %>%
+  filter(year == 2016) %>%
+  mutate(pred = predict(house_lm_pre_2016, newdata = .),
+         residual = pred - margin) %>%
+  summarise(rmse = sqrt(mean(residual^2))) %>%
+  pull(rmse)
